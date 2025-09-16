6dd2e825f079b41a56dc36597e5fffd9b6881612
IT24104143 <IT24104143@my.sliit.lk>
2025-09-16 11:55:10 +0530

    Lab - 06
    Lab - 06

---
+++ Lab Sheet 06/IT24104143_Lab6_PS.R
@@ -0,0 +1,14 @@
+setwd("D:\\PS_lab-06")
+#Question 1
+#i. The distribution of X is a binomial distribution
+#ii.
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+
+#Question 2
+
+#1. number of calls receive by the call center per hour
+
+#2.The distribution is poisson
+
+#3. P(X= 15)
+dpois(15,lambda =12)
